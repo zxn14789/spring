eec88e15275abd0be0883a539a28d37a5b0c7e09
zxn14789 <58654664+zxn14789@users.noreply.github.com>
2019-12-14 10:34:39 +0800

    提交代码到本地

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
